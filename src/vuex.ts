9b645b4dfe37bf3ea3faa1df9914e1ad32c32632
fanwei <fanwei@gaoding.com>
2021-08-03 17:24:24 +0800

    feat(vuex): auto register vuex

--- src/vuex.ts
+++ src/vuex.ts
@@ -1,9 +1,11 @@
-import { Store, StoreOptions } from 'vuex'
+import Vue from 'vue'
+import Vuex, { Store, StoreOptions } from 'vuex'
 import { getCurrentInstance } from '@vue/composition-api'
 import { OUT_OF_SCOPE, warn } from './utils'
 
 
 export function createStore<S>(options: StoreOptions<S>) {
+    Vue.use(Vuex)
     return new Store<S>(options)
 }
 
