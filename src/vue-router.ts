471ac373da12eb8cd1d9756e24157a3734e78fb4
Ambit <ambit_tsai@qq.com>
2022-07-21 11:19:11 +0800

    chore(vue-router): adapt to vue 2.7

--- src/vue-router.ts
+++ src/vue-router.ts
@@ -1,8 +1,6 @@
-import Vue from 'vue'
-import { getCurrentInstance, reactive } from '@vue/composition-api'
-import VueRouter, { NavigationGuard, Route, RouterOptions } from 'vue-router'
-import { OUT_OF_SCOPE, warn } from './utils'
-
+import Vue, { getCurrentInstance, reactive } from 'vue';
+import VueRouter, { NavigationGuard, Route, RouterOptions } from 'vue-router';
+import { OUT_OF_SCOPE, warn } from './utils';
 
 export {
     RouteMeta,
@@ -11,88 +9,82 @@ export {
     RouteConfig as RouteRecordRaw,
     RedirectOption as RouteRecordRedirectOption,
     RawLocation as RouteLocationRaw,
-} from 'vue-router'
-export type RouteRecordName = string | symbol
-export type RouterScrollBehavior = RouterOptions['scrollBehavior']
-export type RouteLocationNormalized = Route
-export type RouteLocationNormalizedLoaded = Route
-
+} from 'vue-router';
+export type RouteRecordName = string | symbol;
+export type RouterScrollBehavior = RouterOptions['scrollBehavior'];
+export type RouteLocationNormalized = Route;
+export type RouteLocationNormalizedLoaded = Route;
 
 export interface Router extends VueRouter {
-    isReady: () => Promise<void>
+    isReady(): Promise<void>;
 
     /** @deprecated */
-    app: VueRouter['app']
+    app: VueRouter['app'];
 
     /** @deprecated */
-    getMatchedComponents: VueRouter['getMatchedComponents']
+    getMatchedComponents: VueRouter['getMatchedComponents'];
 
     /** @deprecated use `isReady` instead */
-    onReady: VueRouter['onReady']
+    onReady: VueRouter['onReady'];
 }
-
 
 // @ts-ignore
 VueRouter.prototype.isReady = function () {
     return new Promise((resolve, reject) => {
-        this.onReady(resolve, reject)
-    })
-}
-
+        this.onReady(resolve, reject);
+    });
+};
 
 export function createRouter(options: RouterOptions) {
-    Vue.use(VueRouter)
-    return new VueRouter(options) as Router
+    Vue.use(VueRouter);
+    return new VueRouter(options) as Router;
 }
-
 
 export function useRouter(): Router {
-    const inst = getCurrentInstance()
+    const inst = getCurrentInstance();
     if (inst) {
-        return inst.proxy.$router as Router
+        return inst.proxy.$router as Router;
     }
-    warn(OUT_OF_SCOPE)
-    return undefined as any
+    warn(OUT_OF_SCOPE);
+    return undefined as any;
 }
 
-
-let currentRoute: RouteLocationNormalizedLoaded
+let currentRoute: RouteLocationNormalizedLoaded;
 
 export function useRoute() {
-    const router = useRouter()
+    const router = useRouter();
     if (!currentRoute) {
-        const inst = getCurrentInstance()
+        const inst = getCurrentInstance();
         if (!inst) {
-            warn(OUT_OF_SCOPE)
-            return
+            warn(OUT_OF_SCOPE);
+            return;
         }
-        currentRoute = reactive({...inst.proxy.$route} as Route)
-        router.afterEach(to => Object.assign(currentRoute, to))
+        currentRoute = reactive({ ...inst.proxy.$route } as Route);
+        router.afterEach((to) => Object.assign(currentRoute, to));
     }
-    return currentRoute
+    return currentRoute;
 }
 
-
 export function onBeforeRouteLeave(leaveGuard: NavigationGuard) {
-    const inst = getCurrentInstance()
+    const inst = getCurrentInstance();
     if (!inst) {
-        warn(OUT_OF_SCOPE)
-        return
+        warn(OUT_OF_SCOPE);
+        return;
     }
-    const { options } = inst.proxy.constructor as any
-    const hooks: any = options.beforeRouteLeave || []
-    hooks.push(leaveGuard)
-    options.beforeRouteLeave = hooks
+    const { options } = inst.proxy.constructor as any;
+    const hooks: any = options.beforeRouteLeave || [];
+    hooks.push(leaveGuard);
+    options.beforeRouteLeave = hooks;
 }
 
 export function onBeforeRouteUpdate(updateGuard: NavigationGuard) {
-    const inst = getCurrentInstance()
+    const inst = getCurrentInstance();
     if (!inst) {
-        warn(OUT_OF_SCOPE)
-        return
+        warn(OUT_OF_SCOPE);
+        return;
     }
-    const { options } = inst.proxy.constructor as any
-    const hooks: any = options.beforeRouteUpdate || []
-    hooks.push(updateGuard)
-    options.beforeRouteUpdate = hooks
+    const { options } = inst.proxy.constructor as any;
+    const hooks: any = options.beforeRouteUpdate || [];
+    hooks.push(updateGuard);
+    options.beforeRouteUpdate = hooks;
 }
