d0abef6c9ad2cb87ba735ca33a9181f3af796fcb
Ambit <ambit_tsai@qq.com>
2022-07-21 11:23:52 +0800

    chore(vuex): adapt to vue 2.7

--- src/vuex.ts
+++ src/vuex.ts
@@ -1,21 +1,18 @@
-import Vue from 'vue'
-import Vuex, { Store, StoreOptions } from 'vuex'
-import { getCurrentInstance } from '@vue/composition-api'
-import { OUT_OF_SCOPE, warn } from './utils'
-
+import Vue, { getCurrentInstance } from 'vue';
+import Vuex, { Store, StoreOptions } from 'vuex';
+import { OUT_OF_SCOPE, warn } from './utils';
 
 export function createStore<S>(options: StoreOptions<S>) {
-    Vue.use(Vuex)
-    return new Store<S>(options)
+    Vue.use(Vuex);
+    return new Store(options);
 }
 
-    
 export function useStore<S = any>(): Store<S> {
-    const inst = getCurrentInstance()
+    const inst = getCurrentInstance();
     if (inst) {
-        return inst.proxy.$store
+        return inst.proxy.$store;
     } else {
-        warn(OUT_OF_SCOPE)
+        warn(OUT_OF_SCOPE);
     }
-    return undefined as any
+    return undefined as any;
 }
